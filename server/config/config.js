075921b90318c4cbf396e723967696c45c118620
Teófilo <teo@rendersoft.pe>
2018-10-10 19:01:44 -0500

    Environment varibale mongo security

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb: //localhost:27017/coffee';
 } else {
-    urlDB = 'mongodb://mlab_user:justdoit10@ds129393.mlab.com:29393/coffee';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
