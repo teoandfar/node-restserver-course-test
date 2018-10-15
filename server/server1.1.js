a5ded8cccda194c76ecac0eeeac49c7d69fd64cd
Teófilo <teo@rendersoft.pe>
2018-10-15 17:20:37 -0500

    Login normal section 10

--- server/server1.1.js
+++ server/server1.1.js
@@ -6,20 +6,19 @@ mongoose.set('useCreateIndex', true);
 const app = express();
 
 const bodyParser = require('body-parser');
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-
-
-// Global setup routes
+// app.use(require('./routes/user').default);
+// app.use(require('./routes/user'));
+// app.use(require('./routes/login'));
 app.use(require('./routes/index'));
 
 
-console.log('server3');
+console.log('server2');
 mongoose.connect(process.env.URL_DB, { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     console.log('Database online');
--- server/server2.js
+++ server/server2.js
@@ -6,19 +6,20 @@ mongoose.set('useCreateIndex', true);
 const app = express();
 
 const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-// app.use(require('./routes/user').default);
-// app.use(require('./routes/user'));
-// app.use(require('./routes/login'));
+
+
+// Global setup routes
 app.use(require('./routes/index'));
 
 
-console.log('server2');
+console.log('server3');
 mongoose.connect(process.env.URL_DB, { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     console.log('Database online');
